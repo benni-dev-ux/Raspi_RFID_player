f20db3fb433ed09e438bcdfde415455ee62a49c7
Benedikt <benediktsteinmueller@googlemail.com>
2021-11-10 21:17:57 +0100

    fixing prototype to create first running example

--- reader.py
+++ reader.py
@@ -1,7 +1,9 @@
 from typing import Match
 import video_player
+import logging
 
 
+# Testfiles
 video1 = ["testvideo", 6267256, "./assets/testvideo.mp4"]
 audio1 = ["testaudio", 6268576, "./assets/testaudio.mp3"]
 
@@ -11,15 +13,25 @@ media_list = []
 media_list.append(video1)
 media_list.append(audio1)
 
-# read and sanitize input
-code = input('Enter your input:')
-code = code[3:]
-code = int(code)
 
-print("Found RFID tag: ")
+def check_for_RFIDMatch(code, media_list):
+    # Check if found code occurs in media list
+    for media in media_list:
+        if(media[1] == code):
+            logging.debug("playing" + media[0] + " at " + media[2])
+            video_player.play_media(media[2])
 
 
-for med in media_list:
-    if(med[1] == code):
-        print("playing" + med[0] + " at " + med[2])
-        video_player.playmovie(med[2])
+def check_for_input():
+    code = input('Enter your input:')
+    # Strip first three Characters to avoid escape characters
+    code = code[3:]
+    # Cast to int
+    code = int(code)
+    return code
+
+
+# Main Loop of the App: Constantly checking for new  RFID input
+while(True):
+    code = check_for_input()
+    check_for_RFIDMatch(code, media_list)
--- video_player.py
+++ video_player.py
@@ -1,7 +1,33 @@
 import subprocess
+import os
+import logging
 
 
-def playmovie(filename):
+def is_playing():
+    """Check if OMX Player is playing a Video (True if at least 2 OMX Processes)"""
+    processname = 'omxplayer'
+    tmp = os.popen("ps -Af").read()
+    proccesscount = tmp.count(processname)
 
+    is_playing = False
+
+    if (proccesscount >= 1):
+        is_playing = True
+
+    return is_playing
+
+
+def play_media(filename):
+
+    global myprocess
+
+    if is_playing():
+        logging.debug("Cancelling currently playing video/audio")
+
+        # Kill existing OMX Processes
+        command1 = "sudo killall -s 9 omxplayer.bin"
+        os.system(command1)
+
+    # starting OMXPlayer subprocess with video or audio file
     myprocess = subprocess.Popen(['omxplayer', filename], stdin=subprocess.PIPE,
                                  stdout=subprocess.PIPE, stderr=subprocess.PIPE, close_fds=True)
